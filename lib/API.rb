c472688c1a5cc2dcd1f810c59e241d0383561318
jjsmokovich <JesseSmokovich@yahoo.com>
2019-05-30 15:56:05 -0700

    seeded the database

--- lib/API.rb
+++ lib/API.rb
@@ -2,30 +2,41 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def api_fetcher
-  url = 'https://opentdb.com/api.php?amount=15&category=25&type=multiple'
-  response = RestClient.get(url)
-  json = JSON.parse(response)
-end
+class API 
+  def initialize
 
-def create_question 
-  api_fetcher['results'].each do |result|
-    quest = result['question']
-    a = result['incorrect_answers'][0]
-    b = result['incorrect_answers'][1]
-    c = result['incorrect_answers'][2]
-    d = result['correct_answer']
-    Question.create(
-      question: quest,
-      correct: a,
-      incorrect1: b,
-      incorrect2: c,
-      incorrect3: d
-    )
+  end
+
+  def run
+    binding.pry
+  end
+  
+  def api_fetcher
+    url = 'https://opentdb.com/api.php?amount=15&category=25&type=multiple'
+    response = RestClient.get(url)
+    json = JSON.parse(response)
+  end
+
+  def create_question 
+    api_fetcher['results'].each do |result|
+      quest = result['question']
+      a = result['incorrect_answers'][0]
+      b = result['incorrect_answers'][1]
+      c = result['incorrect_answers'][2]
+      d = result['correct_answer']
+      # binding.pry
+      Question.create(
+        question: quest,
+        correct: a,
+        incorrect1: b,
+        incorrect2: c,
+        incorrect3: d
+      )
     end
+  end
 end
 
-create_question
+# create_question
 
-binding.pry 
-0
+# binding.pry 
+
