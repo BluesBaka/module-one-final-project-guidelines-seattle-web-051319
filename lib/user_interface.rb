37511690845cb15d6bb4b7484b5bdd3306dcfaa5
Bluesbaka <badunnells@gmail.com>
2019-05-30 09:27:13 -0700

    psuedo code for CLI

---
+++ lib/user_interface.rb
@@ -0,0 +1,13 @@
+##Needs to show a welcome screen
+
+##Needs to ask if the user would like to play
+
+##Needs to provide a selection of options
+
+##Needs to take in the users correct_answer
+
+##Needs to display if the answer was correct or not
+
+##display user record of wins
+
+##invite to play another round or quit
