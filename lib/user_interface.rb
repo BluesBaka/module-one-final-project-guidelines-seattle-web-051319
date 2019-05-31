412ea32347c41cb99a5c0c067396b6b53f71cd6d
Bluesbaka <badunnells@gmail.com>
2019-05-31 09:28:56 -0700

    functionality is improved with the cascade of methods

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -1,20 +1,42 @@
 
 
 class UserInterface
+  attr_accessor :current_user
+  def initialize(current_user=nil)
+    @current_user = current_user
+  end
+
+  def run
+    welcome
+    # binding.pry
+
+    # loop do
+    #   # the main game
+    # end
+  end
+
+
   def welcome
     puts "Welcome"
     puts "You don't know ART"
     sleep(2)
     system("clear")
-    puts "You're in an art trivia game! Answer questions from the prompt."
+    puts "You're in an art trivia game! "
+    puts "Answer questions from the prompt."
     puts "If you'd like to quit, press q."
+    puts
+    puts "What's your name?"
+    users_input
   end
 
   def new_user(user_input)
+    # binding.pry
     username = user_input.downcase.capitalize
-    User.find_or_create_by(username: username)
+    user = User.find_or_create_by(username: username)
+    @current_user = user
     # username = users_input.downcase.capitalize
     #if no user exists
+    game_play
   end
 
 
@@ -26,7 +48,7 @@ class UserInterface
 
 
    # binding.pry
-
+   #displays questions with the correct and incorrect answers
   def display_question(question)
     puts
     puts
@@ -36,34 +58,28 @@ class UserInterface
     puts question.incorrect1
     puts question.incorrect2
     puts question.incorrect3
-
     puts
-
+    puts
   end
 
   def game_play
-
+    is_running = true
+    while is_running
     puts "Would you like to play a couple rounds? Y/N"
-    users_input
-    if users_input == "N"
-    end
-    else
-      question = Question.find_by(id: 2)
+    response = STDIN.gets.chomp.downcase
+    if response == "n"
+      puts "fine, begone with you"
+      is_running = false
+    elsif response =="y"
+      # binding.pry
+      question = Question.find_by(id: 8)
+      Round.create(user: @current_user, question: question)
       display_question(question)
+      end
     end
-  # end
-
-  def run
-    welcome
-
-
-    puts "What's your name?"
-    users_input
-
-    # loop do
-    #   # the main game
-    # end
   end
+
+
   # binding.pry
 end
 
